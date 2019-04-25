afb83d5b153678ed793d34b0ede0bf0a5e5bbdbd
Munsu Ahn <ahnmunsu@gmail.com>
2019-04-25 18:14:38 +0900

    Update index.js

--- air/index.js
+++ air/index.js
@@ -5,6 +5,16 @@ const doc = require('dynamodb-doc');
 const dynamo = new doc.DynamoDB();
 
 
+/**
+ * Demonstrates a simple HTTP endpoint using API Gateway. You have full
+ * access to the request and response payload, including headers and
+ * status code.
+ *
+ * To scan a DynamoDB table, make a GET request with the TableName as a
+ * query string parameter. To put, update, or delete an item, make a POST,
+ * PUT, or DELETE request respectively, passing in the payload to the
+ * DynamoDB API as a JSON body.
+ */
 exports.handler = (event, context, callback) => {
     //console.log('Received event:', JSON.stringify(event, null, 2));
 
@@ -19,9 +29,12 @@ exports.handler = (event, context, callback) => {
     switch (event.httpMethod) {
 
         case 'POST':
+            var body = JSON.parse(event.body);
+            body.Created = new Date().toISOString();
+            
             var params = {
                 TableName: "AirMonitor",
-                Item: JSON.parse(event.body)
+                Item: body
             };
             dynamo.putItem(params, done);
             break;
